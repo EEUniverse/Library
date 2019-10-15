0054cdfceef7bdb2974e49fa177b5561bc3b45fd
SirJosh3917 <joshuaneedham@hotmail.com>
2019-10-14 21:51:18 -0400

    update code to take advantage of faster serializer

--- EEUniverse.Library/Serializer.cs
+++ EEUniverse.Library/Serializer.cs
@@ -20,6 +20,9 @@ namespace EEUniverse.Library
         private const byte _patternObject = 7;
         private const byte _patternObjectEnd = 8;
 
+        /// <summary>
+		/// Calculates the exact size of a message, in bytes.
+		/// </summary>
         public static int EstimateSize(Message message)
         {
 			// currently, a connectionscope & message type can't be above 1 byte when 7 bit encoded.
@@ -99,10 +102,10 @@ namespace EEUniverse.Library
 		/// <summary>
 		/// Convert a message into a stream of bytes.<br />Use with extreme caution.
 		/// </summary>
-		public static Span<byte> SerializeFast(Message message)
+		public static Memory<byte> Serialize(Message message)
         {
-			var target = new Span<byte>(new byte[EstimateSize(message)]);
-			var writer = new MessageWriter(target);
+			var target = new Memory<byte>(new byte[EstimateSize(message)]);
+			var writer = new MessageWriter(target.Span);
 
 			writer.Write(message.Scope);
 			writer.Write(message.Type);
@@ -200,103 +203,6 @@ namespace EEUniverse.Library
 			return target;
 		}
 
-        /// <summary>
-        /// Convert a message into a stream of bytes.<br />Use with caution.
-        /// </summary>
-        public static byte[] Serialize(Message message)
-        {
-            var memStream = new MemoryStream();
-            using var writer = new BitEncodedStreamWriter(memStream);
-            writer.Write7BitEncodedInt((byte)message.Scope);
-            writer.Write7BitEncodedInt((int)message.Type);
-
-            foreach (var data in message) {
-                switch (data) {
-                    case bool oBool: writer.Write(oBool ? _patternBooleanTrue : _patternBooleanFalse); break;
-
-                    case byte oByte:
-                    case sbyte oSByte:
-                    case short oShort:
-                    case int oInt: {
-                            var value = Convert.ToInt32(data);
-                            writer.Write(value < 0 ? _patternIntNeg : _patternIntPos);
-                            writer.Write7BitEncodedInt(value < 0 ? -(value + 1) : value);
-                        }
-                        break;
-
-                    case double oDouble: {
-                            writer.Write(_patternDouble);
-                            writer.Write(oDouble);
-                        }
-                        break;
-
-                    case string oString: {
-                            writer.Write(_patternString);
-                            writer.Write(oString);
-                        }
-                        break;
-
-                    case byte[] oBytes: {
-                            writer.Write(_patternBytes);
-                            writer.Write7BitEncodedInt(oBytes.Length);
-                            writer.Write(oBytes);
-                        }
-                        break;
-
-                    case IDictionary<string, object> oDict: {
-                            writer.Write(_patternObject);
-                            foreach (var kvp in oDict) {
-                                if (kvp.Key.Length == 8) // Until they fix, which will be a breaking change...
-                                    throw new InvalidDataException("The specified key in MessageObject is invalid; it must be lower or greater than 8 characters in length.");
-
-                                writer.Write(kvp.Key);
-                                switch (kvp.Value) {
-                                    case bool oBool: writer.Write(oBool ? _patternBooleanTrue : _patternBooleanFalse); break;
-
-                                    case byte oByte:
-                                    case sbyte oSByte:
-                                    case short oShort:
-                                    case int oInt: {
-                                            var value = Convert.ToInt32(kvp.Value);
-                                            writer.Write(value < 0 ? _patternIntNeg : _patternIntPos);
-                                            writer.Write7BitEncodedInt(value < 0 ? -(value + 1) : value);
-                                        }
-                                        break;
-
-                                    case double oDouble: {
-                                            writer.Write(_patternDouble);
-                                            writer.Write(oDouble);
-                                        }
-                                        break;
-
-                                    case string oString: {
-                                            writer.Write(_patternString);
-                                            writer.Write(oString);
-                                        }
-                                        break;
-
-                                    case byte[] oBytes: {
-                                            writer.Write(_patternBytes);
-                                            writer.Write7BitEncodedInt(oBytes.Length);
-                                            writer.Write(oBytes);
-                                        }
-                                        break;
-
-                                    default: throw new NotSupportedException($"Data type {kvp.Value.GetType().Name} in MessageObject not supported.");
-                                }
-                            }
-
-                            writer.Write(_patternObjectEnd);
-                        }
-                        break;
-
-                    default: throw new NotSupportedException($"Data type {data.GetType().Name} is not supported.");
-                }
-            }
-
-            return memStream.ToArray();
-        }
-
 		/// <summary>
 		/// Deserialize a stream of bytes into a message.<br />Use with extreme caution.
 		/// </summary>
@@ -363,64 +269,5 @@ namespace EEUniverse.Library
 
 			return new Message(scope, type, argData.ToArray());
 		}
-
-        /// <summary>
-        /// Deserialize a stream of bytes into a message.<br />Use with caution.
-        /// </summary>
-        public static Message Deserialize(byte[] data)
-        {
-            using var stream = new BitEncodedStreamReader(new MemoryStream(data));
-            var scope = (ConnectionScope)stream.ReadByte();
-            var type = (MessageType)stream.Read7BitEncodedInt();
-
-            var argData = new List<object>();
-            while (stream.BaseStream.Position < data.Length) {
-                var patternType = stream.ReadByte();
-                switch (patternType) {
-                    case _patternString: argData.Add(stream.ReadString()); break;
-                    case _patternIntPos: argData.Add(stream.Read7BitEncodedInt()); break;
-                    case _patternIntNeg: argData.Add(-stream.Read7BitEncodedInt() - 1); break;
-                    case _patternDouble: argData.Add(BitConverter.ToDouble(stream.ReadBytes(8), 0)); break;
-                    case _patternBooleanFalse: argData.Add(false); break;
-                    case _patternBooleanTrue: argData.Add(true); break;
-                    case _patternBytes: {
-                            var length = stream.Read7BitEncodedInt();
-                            argData.Add(stream.ReadBytes(length));
-                        }
-                        break;
-
-                    case _patternObject: {
-                            var objectArgs = new MessageObject();
-                            while (stream.ReadByte() != _patternObjectEnd) {
-                                stream.BaseStream.Position--;
-
-                                var objectIndex = stream.ReadString();
-                                switch (stream.ReadByte()) {
-                                    case _patternString: objectArgs.Add(objectIndex, stream.ReadString()); break;
-                                    case _patternIntPos: objectArgs.Add(objectIndex, stream.Read7BitEncodedInt()); break;
-                                    case _patternIntNeg: objectArgs.Add(objectIndex, -stream.Read7BitEncodedInt() - 1); break;
-                                    case _patternDouble: objectArgs.Add(objectIndex, BitConverter.ToDouble(stream.ReadBytes(8), 0)); break;
-                                    case _patternBooleanFalse: objectArgs.Add(objectIndex, false); break;
-                                    case _patternBooleanTrue: objectArgs.Add(objectIndex, true); break;
-                                    case _patternBytes: {
-                                            var length = stream.Read7BitEncodedInt();
-                                            objectArgs.Add(objectIndex, stream.ReadBytes(length));
-                                        }
-                                        break;
-
-                                    default: throw new InvalidDataException($"Invalid pattern type {patternType} in MessageObject.");
-                                }
-                            }
-
-                            argData.Add(objectArgs);
-                        }
-                        break;
-
-                    default: throw new InvalidDataException($"Invalid pattern type {patternType}.");
-                }
-            }
-
-            return new Message(scope, type, argData.ToArray());
-        }
     }
 }
